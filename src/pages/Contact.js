e6f1cad30f35fb3d33a51e135fc640746f0b66dc
able24 <egbeleabel@yahoo.com>
2023-02-22 19:53:46 +0000

    added the about section and made the contact form get user input

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -79,7 +79,7 @@ function Contact() {
           <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
             Submit
           </button>
-          {success && <p className ='text-sm'>Feedback Sent Successfully!</p>}
+          {success && <p className ='text-sm text-red-600'>Feedback Sent Successfully!</p>}
         </div>
       </form>
 
